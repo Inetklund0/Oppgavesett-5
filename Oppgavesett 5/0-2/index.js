d39598e21e9dae59f35f971a70df74fb3d1b6dd5
Inetklund0 <56019663+Inetklund0@users.noreply.github.com>
2020-03-19 12:48:09 +0100

    Add files via upload

---
+++ Oppgavesett 5/0-2/index.js
@@ -0,0 +1,9 @@
+const inpPenger = document.querySelector ("#inpPenger")
+
+if ("${inpPenger}"<=500){
+    console.log("${inpPenger}"<=500)
+}
+
+else if("${inpPenger}">500){
+    console.log("For stort beløp")
+}
